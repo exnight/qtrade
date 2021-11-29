4c65b282d24268a91c2ed35fcd931719e4e3ff73
exnight <exnight@users.noreply.github.com>
2021-11-29 12:31:41 +0800

    update indentation

--- src/main/java/com/exnight/backtest/Controller.java
+++ src/main/java/com/exnight/backtest/Controller.java
@@ -18,36 +18,36 @@ import reactor.core.publisher.Flux;
 @RestController
 public class Controller {
 
-  @Getter
-  static class StockPrice {
+    @Getter
+    static class StockPrice {
 
-    private String symbol;
-    private Double price;
-    private LocalDateTime time;
+        private String symbol;
+        private Double price;
+        private LocalDateTime time;
 
-    StockPrice(String symbol, Double price, LocalDateTime time) {
-      this.symbol = symbol;
-      this.price = price;
-      this.time = time;
+        StockPrice(String symbol, Double price, LocalDateTime time) {
+            this.symbol = symbol;
+            this.price = price;
+            this.time = time; // s
+        }
+
     }
 
-  }
+    @GetMapping("/")
+    public String hello() {
+        return "Hello world! Maybe return html and JS here...";
+    }
 
-  @GetMapping("/")
-  public String hello() {
-    return "Hello world! Maybe return html and JS here...";
-  }
+    @RequestMapping(value = "/stock/{symbol}", method = RequestMethod.GET,
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @ResponseBody
+    public Flux<StockPrice> prices(@PathVariable String symbol) {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(n -> new StockPrice(symbol, randomStockPrice(), LocalDateTime.now()));
+    }
 
-  @RequestMapping(value = "/stock/{symbol}", method = RequestMethod.GET,
-      produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  @ResponseBody
-  public Flux<StockPrice> prices(@PathVariable String symbol) {
-    return Flux.interval(Duration.ofSeconds(1))
-        .map(n -> new StockPrice(symbol, randomStockPrice(), LocalDateTime.now()));
-  }
-
-  private Double randomStockPrice() {
-    return ThreadLocalRandom.current().nextDouble(100.0);
-  }
+    private Double randomStockPrice() {
+        return ThreadLocalRandom.current().nextDouble(100.0);
+    }
 
 }
